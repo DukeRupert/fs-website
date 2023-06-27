9a3c9519db7fd0ea8df207c6f25af464ca729b48
Logan <logan@firefly.llc>
2023-06-27 11:31:07 -0600

    Updated image format in Hero component.

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -116,7 +116,7 @@
             clip-path="url(#2ade4387-9c63-4fc4-b754-10e687a0d332)"
           >
             <img
-              src="/images/screencapture-kagenandco-2023-06-11-08_26_37.png"
+              src="/images/screencapture-kagenandco-2023-06-11-08_26_37.webp"
               alt="Kagen Coffee and Crepes mobile website screen capture"
               width="488"
               height="1053"
