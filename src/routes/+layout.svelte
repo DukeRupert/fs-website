4e42b71735bf8935aa5955b48e00fdcd2271e6ff
Logan <logan@firefly.llc>
2023-06-30 07:42:09 -0600

    Added Vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
   import "../app.postcss";
   import Navigation from "$lib/components/navigation/TopBar.svelte";
   import Footer from "$lib/components/Footer.svelte";
+
+  // Vercel Analytics
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
