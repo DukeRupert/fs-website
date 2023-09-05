29c8a30a1770894fdbd3ae0ee9194c134761db12
Logan <logan@firefly.llc>
2023-09-05 23:30:49 +0000

    Working on Posts

---
+++ src/lib/components/posts/PostGrid.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import PostCard from "./PostCard.svelte";
+    interface Post {
+        title: string;
+    }
+
+    export let posts: Post[];
+</script>
+<div
+  class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+>
+  {#each posts as post}
+    <PostCard {post} />
+  {/each}
+</div>
