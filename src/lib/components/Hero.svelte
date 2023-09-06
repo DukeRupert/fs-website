bd0fe08eb82c4e249c0bde3a3644eb957873b001
Logan <logan@firefly.llc>
2023-09-06 01:51:45 +0000

    Updated primary action in Hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -80,7 +80,7 @@
         </p>
         <div class="mt-10 flex items-center gap-x-6">
           <a
-            href="#contact-us"
+            href="/contact-us"
             class="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
             >Get started</a
           >
