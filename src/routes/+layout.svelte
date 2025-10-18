3244bd0c21d5288f5b633bd0bfa2ecf6de5ff09c
Logan Williams <logan@fireflysoftware.dev>
2025-10-17 20:49:27 -0600

    refactored for node deployement

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,6 @@
   import "../app.postcss";
   import Navigation from "$lib/components/navigation/TopBar.svelte";
   import Footer from "$lib/components/Footer.svelte";
-
-  // Vercel Analytics
-  import { dev } from "$app/environment";
-  import { inject } from "@vercel/analytics";
-  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head />
